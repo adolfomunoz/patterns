5c820f6893e628c1c8643b9054aa0743cf3c1bcc
adolfomunoz <adolfo@unizar.es>
2021-11-17 16:56:39 +0100

    Starting implementation of polymorphic reflection.

--- src/self-registering-reflect.h
+++ src/self-registering-reflect.h
@@ -11,13 +11,25 @@ namespace pattern {
    
 template<typename Self>
 class SelfRegisteringReflectableBase {
-public:
-    virtual std::string xml_content() const = 0;
+protected:
+    virtual std::string xml_content(const std::string& prefix = "") const = 0;
+    virtual void load_content(rapidxml::xml_node<>* node) = 0;
 };
 
 template<typename Self, typename... Bases>
 class SelfRegisteringReflectable : public SelfRegisteringClass<Self, Bases...>, public Reflectable<Self,Bases...> {
-        std::
+protected:
+    std::string xml_content(const std::string& prefix = "") const override {
+        std::stringstream sstr;
+        t.for_each_attribute([&sstr,&prefix] (const std::string& name, const auto& value) {
+            sstr<<XML<std::decay_t<decltype(value)>>::get(value,name,prefix+"   ");
+        });
+    }
+    void load_content(rapidxml::xml_node<>* node) override {
+        t.for_each_attribute([&node] (const std::string& name, auto& value) {
+            XML<std::decay_t<decltype(value)>>::load(value,node,name);
+        });          
+    }
 };
 
 
