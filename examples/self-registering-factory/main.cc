5602ab057a21b8e36d60be5e0ab3682a4b4ce486
Adolfo <adolfo@unizar.es>
2025-01-22 14:33:39 +0100

    Updated executable to adapt new way of accounting for registered stuff.

--- examples/self-registering-factory/main.cc
+++ examples/self-registering-factory/main.cc
@@ -4,10 +4,11 @@
 
 int main(int argc, char** argv) {
     if (argc>1) {
-        if (auto shape = pattern::SelfRegisteringFactory<Shape>::make_unique(argv[1]))
+        if (auto shape = pattern::SelfRegisteringFactory<Shape>::make_unique(argv[1])) {
             std::cout<<shape->area()<<std::endl;
-        else
-            std::cout<<pattern::SelfRegisteringFactory<Shape>::registered()<<std::endl;
-    } else 
-        std::cout<<pattern::SelfRegisteringFactory<Shape>::registered()<<std::endl;  
+            return 0;
+        }
+    }
+    for (auto r : pattern::SelfRegisteringFactory<Shape>::registered()) std::cout<<r<<" ";
+    std::cout << std::endl;
 }
