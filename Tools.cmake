acc37b95036ab677671cd441f3bbe3e847547323
Adolfo <adolfo@unizar.es>
2025-12-11 13:39:54 +0100

    Modified cmake so it can be included from elsewhere.

--- Tools.cmake
+++ Tools.cmake
@@ -1,5 +1,5 @@
 ###################################################################################
 # TOOLS FROM THE LIBRARY THAT ARE HELPFUL IN OTHER LIBRARIES
 ###################################################################################
-add_executable(dylib-contents tools/dylib-contents/main.cc)
+add_executable(dylib-contents ${CMAKE_CURRENT_LIST_DIR}/tools/dylib-contents/main.cc)
 
